c62a69bbc710118c5e6848bb31e5aaaa1041574d
kstamaria <36648654+kstamaria@users.noreply.github.com>
2018-02-19 18:54:28 -0500

    added a couple lines of output and a comment

--- HelloWorld/src/helloWorld.java
+++ HelloWorld/src/helloWorld.java
@@ -4,7 +4,8 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("How do i do this?");
-
+		System.out.println("THIS IS TESTING AND IDK WHAT'S HAPPENING");
+		//I hope this is right
 	}
 
 }
