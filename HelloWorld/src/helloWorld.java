728f4a8a164e5a854d7845a5d6cc58d96053927b
Fred Gortler <fred.gortler@gmail.com>
2018-02-20 10:10:56 -0500

    Updated HelloWorld to reflect feelings on life.

--- HelloWorld/src/helloWorld.java
+++ HelloWorld/src/helloWorld.java
@@ -4,6 +4,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("How do i do this?");
+		System.out.println("Existance is pain.");
 
 	}
 
